ea30d0baa683d936748e64ba3b9b904c6cb78425
Hannes Graf <hannes.graf@onsite.ch>
2023-11-15 14:09:54 +0100

    comment gnome display

--- hosts/libelula/default.nix
+++ hosts/libelula/default.nix
@@ -64,8 +64,8 @@
   services.flatpak.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+#  services.xserver.displayManager.gdm.enable = true;
+#  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver = {
