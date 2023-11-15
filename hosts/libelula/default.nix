cc051b6a626458ef626eab0b8373f57f030705ce
Hannes Graf <hannes.graf@onsite.ch>
2023-11-15 14:30:36 +0100

    imports

--- hosts/libelula/default.nix
+++ hosts/libelula/default.nix
@@ -21,13 +21,14 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ../../modules/desktops/virtualisation/docker.nix
+    ] ++
     ( import ../modules/desktops ++
-                  import ../modules/editors ++
-                  import ../modules/hardware ++
-                  import ../modules/programs ++
-                  import ../modules/services ++
-                  import ../modules/shell ++
-                  import ../modules/theming )];
+                      import ../modules/editors ++
+                      import ../modules/hardware ++
+                      import ../modules/programs ++
+                      import ../modules/services ++
+                      import ../modules/shell ++
+                      import ../modules/theming );
 
   # Bootloader.
   # boot.loader.systemd-boot.enable = true;
