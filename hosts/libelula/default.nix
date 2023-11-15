9f4a788de37b25d828b9d84f86d4a1e99b767039
Hannes Graf <hannes.graf@onsite.ch>
2023-11-15 13:56:41 +0100

    try radical overwrite

--- hosts/libelula/default.nix
+++ hosts/libelula/default.nix
@@ -17,41 +17,242 @@
 { pkgs, ... }:
 
 {
-  imports = [
-    ./hardware-configuration.nix
-    ../../modules/desktops/virtualisation/docker.nix
-  ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
 
-  boot = {                                  # Boot Options
-    loader = {
-      efi = {
-        canTouchEfiVariables = true;
-        efiSysMountPoint = "/boot";
-      };
-      grub = {                              # Grub Dual Boot
-        enable = true;
-        devices = [ "nodev" ];
-        efiSupport = true;
-        useOSProber = true;                 # Find All Boot Options
-        configurationLimit = 2;
-      };
-      timeout = 1;
+  # Bootloader.
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.grub.useOSProber = true;
+
+  boot.loader = {
+    grub = {
+      enable = true;
+      efiSupport = true;
+      enableCryptodisk = true;
+      device = "nodev";
+      useOSProber = true;
     };
-    kernelPackages = pkgs.linuxPackages_latest;
+    efi.canTouchEfiVariables = true;
   };
 
-  hardware.sane = {                         # Scanning
+  networking.hostName = "libelula"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+
+  services.strongswan.enable = true;
+
+
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Zurich";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.flatpak.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "ch";
+    xkbVariant = "de_nodeadkeys";
+  };
+
+  # Configure console keymap
+  console.keyMap = "sg";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
     enable = true;
-    extraBackends = [ pkgs.sane-airscan ];
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
   };
 
-  laptop.enable = true;                     # Laptop Modules
-  bspwm.enable = true;                      # Window Manager
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
-  environment = {
-    systemPackages = with pkgs; [           # System-Wide Packages
-      simple-scan       # Scanning
-      onlyoffice-bin    # Office
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.caldetas = {
+    isNormalUser = true;
+    description = "caldetas";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      firefox
+      pciutils
+    #  thunderbird
     ];
   };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  ### graphic card
+
+
+
+  boot.kernelParams = [ "psmouse.synaptics_intertouch=0" ];
+  #hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable
+  services.xserver.videoDrivers = [ "nvidia" ];
+  #hardware.opengl.driSupport32Bit = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  wget
+  brave
+  htop
+  gparted
+  jetbrains.idea-ultimate
+#  jetbrains.plugins.addplugins
+#  jetbrains.idea-ultimate["github-copilot"]
+  gnome.gedit
+  gnome.gnome-tweaks
+  yaru-theme
+  git
+  jdk17
+  nodejs_16
+  docker-compose
+  telegram-desktop
+  networkmanager_strongswan
+  openvpn
+  stremio
+  qbittorrent
+  mesa #elden ring
+  directx-headers #elden ring
+  directx-shader-compiler #elden ring
+  ];
+
+ #copilot
+# (jetbrains.plugins.addPlugins jetbrains.clion [ "github-copilot" ])
+
+#environment.etc = {
+#  "xdg/gtk-2.0/gtkrc".text = "gtk-error-bell=0";
+#  "xdg/gtk-3.0/settings.ini".text = ''
+#    [Settings]
+#    gtk-error-bell=false
+#  '';
+#  "xdg/gtk-4.0/settings.ini".text = ''
+#    [Settings]
+#    gtk-error-bell=false
+#  '';
+#};
+
+
+  #Steam
+  #programs.steam.enable = true;
+  programs.gamemode.enable = true;
+
+  #java
+  programs.java.enable = true;
+  nixpkgs.config.permittedInsecurePackages = [
+                "nodejs-16.20.2"
+              ];
+  virtualisation.docker.enable = true;
+
+  #Shortcuts
+  programs.openvpn3.enable = true;
+
+  programs.dconf = {
+  enable = true;
+  };
+
+#  qt = {
+#    enable = true;
+#    platformTheme = "gnome";
+#    style = "adwaita-dark";
+#  };
+
+
+    # Enable OpenGL
+    hardware.opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+    };
+
+    hardware.nvidia = {
+
+      # Modesetting is required.
+      modesetting.enable = true;
+
+      # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+      powerManagement.enable = false;
+      # Fine-grained power management. Turns off GPU when not in use.
+      # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+      powerManagement.finegrained = false;
+
+      # Use the NVidia open source kernel module (not to be confused with the
+      # independent third-party "nouveau" open source driver).
+      # Support is limited to the Turing and later architectures. Full list of
+      # supported GPUs is at:
+      # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
+      # Only available from driver 515.43.04+
+      # Currently alpha-quality/buggy, so false is currently the recommended setting.
+      open = false;
+
+      # Enable the Nvidia settings menu,
+  	# accessible via `nvidia-settings`.
+      nvidiaSettings = true;
+
+      # Optionally, you may need to select the appropriate driver version for your specific GPU.
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
+
 }
+
