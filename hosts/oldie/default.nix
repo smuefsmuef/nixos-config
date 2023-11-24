6347b3abab87072b662c22f7cc702139a7da8918
caldetas <misterhannes@hotmail.com>
2023-11-24 15:34:38 +0100

    oldie try hypr

--- hosts/oldie/default.nix
+++ hosts/oldie/default.nix
@@ -56,7 +56,6 @@
       qt5ct
       libva
     ];
-  };
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
