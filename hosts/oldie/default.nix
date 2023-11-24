bf68078e84f0dfc4d25ac4a89720a9f2dbcd93ea
caldetas <misterhannes@hotmail.com>
2023-11-24 14:36:58 +0100

    oldie try hypr

--- hosts/oldie/default.nix
+++ hosts/oldie/default.nix
@@ -18,7 +18,7 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ (modulesPath + "/installer/scan/not-detected.nix")
       ./hardware-configuration.nix
       ../../modules/desktops/virtualisation/docker.nix
     ] ++
@@ -31,110 +31,50 @@
                       import ../../modules/theming
                       );
 
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
+  boot.initrd.kernelModules = [ ];
+  boot.blacklistedKernelModules = [ "nouveau" "nvidia" ];
+  boot.kernelParams = [ "i915.enable_guc=2" ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
-  # Enable the X11 windowing system. (gnome?)
-# services.xserver.enable = true;
-# bspwm.enable = true;
-# wlwm.enable = true;
-#  hyprland.enable = true;
-#services.xserver.desktopManager.gnome.enable = true;
-#laptop.enable = true;                     # Laptop Modules, conflicts with tlp.enable = true
-
-gnome.enable = true;
-#x11wm.enable = true;                      # Window Manager
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
 
 
-  # Configure keymap in X11
-#  services.xserver = {
-#    layout = "ch";
-#    xkbVariant = "de_nodeadkeys";
-#  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/18fd3508-94a9-4d44-8b18-d7971fbb86c5";
+      fsType = "ext4";
+    };
+
+      swapDevices = [ ];
 
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
+  # (the default) this is the recommended approach. When using systemd-networkd it's
+  # still possible to use this option, but it's recommended to use it in conjunction
+  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
+  networking.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp0s20f3.useDHCP = lib.mkDefault true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
   };
-
-/*
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-
-  mesa #elden ring
-  directx-headers #elden ring
-  directx-shader-compiler #elden ring
-  ];
-
-  #Steam
-  programs.steam.enable = true;
-  programs.gamemode.enable = true;
-
-  #java
-  programs.java.enable = true;
-  nixpkgs.config.permittedInsecurePackages = [
-                "nodejs-16.20.2"
-              ];
-  virtualisation.docker.enable = true;
-
-  #Shortcuts
-#  programs.openvpn3.enable = true;
-
-#  programs.dconf = {
-#  enable = true;
-#  };
-
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-*/
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.05"; # Did you read the comment?
-
-}
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
+  }
 
