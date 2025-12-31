f1b5d93221614a5f6cb4a1d11875ebcae3e63050
kohler <smuefsmuef@gmail.com>
2025-12-31 11:36:29 +0100

    problems with home manager

--- shells/unstable/flake.nix
+++ shells/unstable/flake.nix
@@ -6,7 +6,7 @@
   description = "Use stable and unstable packages";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 
