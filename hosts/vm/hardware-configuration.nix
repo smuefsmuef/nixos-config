28acc8dc273d996cce66c88afc121017462ee061
kohler <smuefsmuef@gmail.com>
2025-12-31 10:29:34 +0100

    chore(nvidia): remove deprecated hardware.opengl.driSupport options

--- hosts/vm/hardware-configuration.nix
+++ hosts/vm/hardware-configuration.nix
@@ -34,7 +34,6 @@
 
     hardware.opengl = {
       enable = true;
-      driSupport = true;
       extraPackages = with pkgs; [
         intel-compute-runtime
         intel-media-driver # LIBVA_DRIVER_NAME=iHD
